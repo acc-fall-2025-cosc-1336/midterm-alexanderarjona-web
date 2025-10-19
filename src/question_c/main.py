ebd6a7f5cf55cc5d7874331648c14eb8b6696689
Alexander Arjona <alexander.arjona@g.austincc.edu>
2025-10-18 22:02:52 -0500

    Update main.py

--- src/question_c/main.py
+++ src/question_c/main.py
@@ -1,1 +1,8 @@
-#add import
+#write functions here, don't add input('') statements here!
+
+def use_local_variable(num):
+    # local variable inside function
+    num = 10  # this does not affect outside variable
+
+def test_config():
+    return True#add import
