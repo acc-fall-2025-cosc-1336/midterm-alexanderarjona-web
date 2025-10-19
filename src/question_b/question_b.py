160491993798ab83ae556bb6715dc7902057d2d7
Alexander Arjona <alexander.arjona@g.austincc.edu>
2025-10-18 22:02:38 -0500

    Update question_b.py

--- src/question_b/question_b.py
+++ src/question_b/question_b.py
@@ -1,1 +1,9 @@
 #write functions here, don't add input('') statements here!
+import random
+
+def get_random_number():
+    # generates random number between 1 and 5
+    return random.randint(1, 5)
+
+def test_config():
+    return True
