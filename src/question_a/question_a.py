a37e4cc89ae37e4f19af17196e801a964286562b
Alexander Arjona <alexander.arjona@g.austincc.edu>
2025-10-18 22:02:23 -0500

    Update question_a.py

--- src/question_a/question_a.py
+++ src/question_a/question_a.py
@@ -1,3 +1,12 @@
 #write functions here, don't add input('') statements here!
+
+# global variable
+count = 0
+
+def use_global():
+    global count
+    count = count + 1
+    return count
+
 def test_config():
     return True
